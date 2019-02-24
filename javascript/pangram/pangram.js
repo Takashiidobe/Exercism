2142cfae0e14b89cc8112fa450e065998761064d
takashiidobe <idobetakashi@gmail.com>
2019-02-24 15:46:25 -0600

    finished pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,15 +1,15 @@
-const isPangram = str => {
-  str = str.toLowerCase();
-  //we need to construct a pangram, which contains one of every letter in the alphabet.
-  //we can loop through the entire string, and create a map of every letter in the alphabet, and see if that matches.
-  //while we loop through the string, lets loop through.
-  const hashmap = {};
-  for (let ltr of str) {
-    if (!hashmap[ltr]) hashmap[ltr] = 1;
+const isPangram = string => {
+  if (string.length < 26) return false;
+  const pangram = {};
+  let parsed = string.toLowerCase().replace(/[^a-z]/gi, '');
+  for (let letter of parsed) {
+    if (!pangram[letter]) {
+      pangram[letter] = 1;
+    }
   }
-  console.log(Object.keys(hashmap));
-  const alphabetstring = 'abcdefghijklmnopqrstuvwxyz';
-  const alphabetarray = alphabetstring.split('');
+  return Object.keys(pangram).length === 26 ? true : false;
 };
 
-export default isPangram;
+module.exports = {
+  isPangram
+};
